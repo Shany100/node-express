0a4b9c52d2d7017e148b57ab366360b0f1e042ae
jantollong@gmail.com <jantollong@gmail.com>
2015-07-10 17:12:06 +0800

    change reade.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# nodejs express sample
-##nodejs express sample test
+## Node.js 项目实践
+
+
+##### 项目结构
+```
+node-express/
+  config/
+  controllers/
+  models/
+  public/
+  routers/
+  views/
+  test/
+    - data.json
+  .gitignore
+  index.html
+  package.json
+  README.md
+
+```
---
+++ articles.js
@@ -0,0 +1,83 @@
+
+module.exports = {
+	articles: [{
+		name: '2015：巨型 JavaScript 框架的末日',
+		url: 'http://web.jobbole.com/82388/'
+	},{
+		name: "2015前端框架何去何从",
+		url: 'http://www.cnblogs.com/sskyy/p/4264371.html'
+	},{
+		name: '2015前端组件化框架之路',
+		url: 'https://github.com/xufei/blog/issues/19'
+	},{
+		name: '去哪儿网前端架构师司徒正美：如何挑选适合的前端框架？',
+		url: 'http://www.csdn.net/article/2015-05-11/2824656-fontend-Frameworks'
+	},{
+		name: '移动端开发所需要的一些资源与小技巧',
+		url: 'https://github.com/jtyjty99999/mobileTech'
+	},{
+		name: '百度吴多溢-移动端跨平台开发的各种技术',
+		url: 'https://github.com/jtyjty99999/mobileTech'
+	},{
+		name: '来自Heroku团队的：当下构建Web app的12条最佳实践',
+		url: 'http://12factor.net/zh_cn/'
+	},{
+		name: '前端架构',
+		url: 'http://saito.im/note/The-Architecture-of-F2E/'
+	}],
+	frames: [{
+		name: "Vue",
+		version: "0.11.10",
+		size: "181.31kb",
+		minSize: "56.08kb minified / 18.39kb gzipped",
+		browser: "不支持IE8及以下版本"
+	},{
+		name: "React",
+		version: "0.13.0",
+		size: "585kb",
+		minSize: "118kb minified",
+		browser: "支持IE8及以上版本"
+	},{
+		name: "Angular",
+		version: "0",
+		size: "0",
+		minSize: "0",
+		browser: "0"
+	},{
+		name: "Knockout",
+		version: "0",
+		size: "0",
+		minSize: "0",
+		browser: "0"
+	},{
+		name: "Ember",
+		version: "0",
+		size: "0",
+		minSize: "0",
+		browser: "0"
+	},{
+		name: "Ext",
+		version: "0",
+		size: "0",
+		minSize: "0",
+		browser: "0"
+	},{
+		name: "Backbone",
+		version: "0",
+		size: "0",
+		minSize: "0",
+		browser: "0"
+	},{
+		name: "jQuery",
+		version: "0",
+		size: "0",
+		minSize: "0",
+		browser: "0"
+	},{
+		name: "Zepto",
+		version: "0",
+		size: "0",
+		minSize: "0",
+		browser: "0"
+	}]
+}
